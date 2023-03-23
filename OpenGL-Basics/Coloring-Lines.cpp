843ea448066732eea4a2c8c884032b8b0e14adfe
Manish <111450082+ManishModak@users.noreply.github.com>
2023-03-23 15:02:27 +0530

    Update Coloring-Lines.cpp

--- OpenGL-Basics/Coloring-Lines.cpp
+++ OpenGL-Basics/Coloring-Lines.cpp
@@ -1,1 +1,48 @@
+#include<GL/glut.h>
 
+void init()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+void display()
+{
+	glColor3f(1,0,0);
+	
+	glPointSize(10);
+	glBegin(GL_LINE_STRIP) ;
+	glVertex2f(0.5,0.5);
+	glVertex2f(0.0,0.5);
+	glVertex2f(0.0,-0.5);
+	glVertex2f(-0.5,-0.5);
+	glEnd() ;
+	glFlush();
+	
+	glPointSize(10);
+	glBegin(GL_LINE_STRIP) ;
+	glVertex2f(-0.5,0.5);
+	glVertex2f(-0.5,0.0);
+	glVertex2f(0.5,0.0);
+	glVertex2f(0.5,-0.5);
+	glEnd() ;
+	glFlush();
+	
+}
+
+int main(int argc,char** argv)
+{
+	glutInit(&argc , argv);
+	glutInitWindowSize(1600,900);
+	glutInitWindowPosition(0,0);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB) ;
+	glutCreateWindow("Sample2") ;
+	
+	glClearColor(1.0,0.0,0.0);
+	init();
+	glutDisplayFunc(display);
+	
+	glutMainLoop() ;
+	
+	return 0 ;
+}
